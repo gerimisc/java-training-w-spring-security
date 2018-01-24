cc4ce0b6cd6412aaa4c27918d82c95b1ff6e824e
Audi Sugianto (Lazada Group) <audi.sugianto@lazada.com>
2018-01-24 16:25:21 +0800

    edited index page

--- spring/src/main/java/com/vulnerable/web/spring/index/IndexService.java
+++ spring/src/main/java/com/vulnerable/web/spring/index/IndexService.java
@@ -16,8 +16,12 @@ public class IndexService {
 		indexes.add(new Index("<a href=/xss/reflected-2>Reflected XSS (POST)</a>", "<a href=/xss/reflected-soln-2>Spring MVC Tags</a>"));
 		indexes.add(new Index("<a href=/xss/stored>Stored XSS </a>", "<a href=/xss/reflected>Spring module.. </a>"));
 		indexes.add(new Index("<a href=/xss/dom>DOM XSS </a>", "<a href=/xss/reflected>Spring module.. </a>"));
-		indexes.add(new Index("<a href=/file/upload>Unrestricted File Upload</a>", "<a href=/file/upload-soln>Whitelisting of Content Type</a>"));
-		indexes.add(new Index("<a href=/index>Directory Traversal</a>", "<a href=/xss/reflected>Spring module.. </a>"));
+		indexes.add(new Index("<a href=/file/upload>Unrestricted File Upload</a>", "<a href=/file/upload-soln>Whitelisting of Content Type</a><br/>"
+				+ "String org.springframework.web.multipart.MultipartFile.getContentType()<br/>"
+				+ "<a>Whitelisting of file extensions</a><br/>"
+				+ "org.apache.commons.io.FilenameUtils"));
+		indexes.add(new Index("<a href=/file/upload>Directory Traversal</a>", "<a href=/file/upload-soln>Strips '/' </a><br/>"
+				+ "String org.springframework.web.multipart.MultipartFile.getOriginalFilename()"));
 		indexes.add(new Index("<a href=/index>SQL Injection</a>", "<a href=/xss/reflected>Spring module.. </a>"));
 	}
 	
