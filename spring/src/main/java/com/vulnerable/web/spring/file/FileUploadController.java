67b684c0b22ab240c7e7025f0153cb614ab81025
Audi Sugianto (Lazada Group) <audi.sugianto@lazada.com>
2018-01-24 11:36:06 +0800

    added soln for file upload. checks content-type

--- spring/src/main/java/com/vulnerable/web/spring/file/FileUploadController.java
+++ spring/src/main/java/com/vulnerable/web/spring/file/FileUploadController.java
@@ -29,6 +29,11 @@ public class FileUploadController {
 		return "upload";
 	}
 	
+	@RequestMapping(value = "/upload-soln", method = RequestMethod.GET)
+	public String uploadFilePageSoln() {
+		return "upload-soln";
+	}
+	
 	@RequestMapping(value = "/upload", method = RequestMethod.POST)
 	public String uploadFileHandler(ModelMap model, @RequestParam("file") MultipartFile file) {
 		if (!file.isEmpty()) {
@@ -54,6 +59,45 @@ public class FileUploadController {
 				String path = "File has been uploaded to <b>" + serverFile;
 				model.addAttribute("path", path);
 				return "upload";
+			} catch (Exception e) {
+				return "You failed to upload " + "" + " => " + e.getMessage();
+			}
+		} else {
+			return "You failed to upload " + ""
+					+ " because the file was empty.";
+		}
+	}
+	
+	@RequestMapping(value = "/upload-soln", method = RequestMethod.POST)
+	public String uploadFileHandlerWhitelist(ModelMap model, @RequestParam("file") MultipartFile file) {
+		if(!file.getContentType().equalsIgnoreCase("application/vnd.openxmlformats-officedocument.wordprocessingml.document")){
+            String error = "File not supported!";
+            model.addAttribute("path", error);
+			return "upload-soln";
+        }
+		if (!file.isEmpty()) {
+			try {
+				byte[] bytes = file.getBytes();
+				String name = file.getOriginalFilename();
+				// Creating the directory to store file
+				String rootPath = System.getProperty("catalina.home");
+				File dir = new File(rootPath + File.separator + "tmpFiles");
+				if (!dir.exists())
+					dir.mkdirs();
+
+				// Create the file on server
+				File serverFile = new File(dir.getAbsolutePath()
+						+ File.separator + name);
+				BufferedOutputStream stream = new BufferedOutputStream(
+						new FileOutputStream(serverFile));
+				stream.write(bytes);
+				stream.close();
+
+//				logger.info("Server File Location="
+//						+ serverFile.getAbsolutePath());
+				String path = "File has been uploaded to <b>" + serverFile;
+				model.addAttribute("path", path);
+				return "upload-soln";
 			} catch (Exception e) {
 				return "You failed to upload " + "" + " => " + e.getMessage();
 			}
---
+++ spring/src/main/webapp/WEB-INF/views/upload-soln.jsp
@@ -0,0 +1,28 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" %>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>File Upload</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+</head>
+<body>
+<h2>Unrestricted File Upload</h2>
+
+<form method="POST" action="/file/upload-soln" enctype="multipart/form-data">
+  <div class="form-group">
+    <label for="resume">CV/Resume</label>
+	<input type="file" class="form-control-file" name="file">
+	<small id="uploadHelp" class="form-text text-muted">.txt, .doc, .docx files are accepted</small>
+</div>
+  <button type="submit" class="btn btn-primary">Upload</button>
+</form>
+${path}
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</body>
+</html>
